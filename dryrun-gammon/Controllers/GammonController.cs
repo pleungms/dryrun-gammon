e68c28b2d1621b752b84da63805c915334d93c72
Peter Leung <peleun@microsoft.com>
2020-07-21 09:50:03 +0800

    Add project files.

---
+++ dryrun-gammon/Controllers/GammonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dryrun_gammon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GammonController : ControllerBase
+    {
+        public string Get(int id)
+        {
+            return "Hello World!";
+        }
+
+    }
+}
